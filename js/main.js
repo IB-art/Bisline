fd94b9620a675ca03099e784b40f677f8c078245
IB-art <65600199+IB-art@users.noreply.github.com>
2021-06-09 00:03:41 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+
+  $('.slider__inner').slick({
+    arrows: false,
+    dots: true,
+  });
+
+  $('.works__inner').slick({
+    slidesToShow: 4,
+   slidesToScroll: 1,
+   prevArrow: '<button class="slick-arrow slick-prev"><img src="images/slider/slide-left.png" alt=""></button>',
+   nextArrow: '<button class="slick-arrow slick-next"><img src="images/slider/slide-right.png" alt=""></button>',
+   responsive: [
+     {breakpoint:911,
+      settings: {
+        slidesToShow: 3,
+      }
+   },
+      {breakpoint:700,
+       settings: {
+        slidesToShow: 2,
+      }
+   },
+      {breakpoint:480,
+       settings: {
+        slidesToShow: 1,
+      }
+   },
+  ]
+  });
+
+
+  $('.header__menu-btn').on('click', function(){
+    $('.header__menu > ul').slideToggle();
+  });
+
+  $('.header__drop-down.drop-down').on('click', function(){
+    $(this).children('.drop-down__list').toggleClass('active');
+    $(this).children('.drop-down__link').toggleClass('active');
+  });
+
+
+});
